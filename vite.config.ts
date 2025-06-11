b93165d491de13eb6dfe61a3e1da4ce6f6e35e33
sebatyler <sebatyler@gmail.com>
2025-06-11 16:38:22 +0700

    fix: switch to hash router

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "ghpages" ? "/PKfriends/" : "/",
+  base: mode === "ghpages" ? "" : "/",
   server: {
     host: "::",
     port: 3000,
