cd905f760393b0bc9c59b12fd92df1ab52453490
sebatyler <sebatyler@gmail.com>
2025-06-11 16:08:21 +0700

    feat: add ghpages build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "ghpages" ? "/PKfriends/" : "/",
   server: {
     host: "::",
     port: 3000,
