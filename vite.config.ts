67a3dc28669dc84d54c0cc2f88b9e35f02774efa
team@flow <94040508+bssanyang@users.noreply.github.com>
2025-06-20 18:16:48 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "ghpages" ? "" : "/",
   server: {
     host: "::",
-    port: 3000,
+    port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
