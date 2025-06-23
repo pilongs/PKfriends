70b6082b3ff5b5b488d135ae2070401b3f085402
team@flow <stacey@atflow.kr>
2025-06-23 10:23:07 +0900

    chore: add base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "ghpages" ? "" : "/",
+  base: "/PKfriends/",
   server: {
     host: "::",
     port: 3000,
